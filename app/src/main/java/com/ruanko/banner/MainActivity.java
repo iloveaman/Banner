c508d7fccf399838c2f03f443381bb9e14fd95fe
iloveaman <iloveaman@163.com>
2015-06-01 12:48:14 +0800

    initial commit

---
+++ app/src/main/java/com/ruanko/banner/MainActivity.java
@@ -0,0 +1,42 @@
+package com.ruanko.banner;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+import com.ruanko.banner.library.BGABanner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.bingoogolapple.bgaannotation.BGAA;
+import cn.bingoogolapple.bgaannotation.BGAALayout;
+import cn.bingoogolapple.bgaannotation.BGAAView;
+
+@BGAALayout(R.layout.activity_main)
+public class MainActivity extends FragmentActivity {
+    @BGAAView(R.id.banner1)
+    private BGABanner mBanner1;
+    @BGAAView(R.id.banner2)
+    private BGABanner mBanner2;
+    @BGAAView(R.id.banner3)
+    private BGABanner mBanner3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BGAA.injectView2Activity(this);
+        mBanner1.setViewPagerViews(getDatas());
+        mBanner2.setViewPagerViews(getDatas());
+        mBanner3.setViewPagerViews(getDatas());
+    }
+
+    private List<View> getDatas() {
+        List<View> datas = new ArrayList<View>();
+        datas.add(getLayoutInflater().inflate(R.layout.view_one, null));
+        datas.add(getLayoutInflater().inflate(R.layout.view_two, null));
+        datas.add(getLayoutInflater().inflate(R.layout.view_three, null));
+        datas.add(getLayoutInflater().inflate(R.layout.view_four, null));
+        return datas;
+    }
+}
---
+++ app/src/main/java/com/ruanko/banner/SplashActivity.java
@@ -0,0 +1,44 @@
+package com.ruanko.banner;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+
+import com.ruanko.banner.library.BGABanner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.bingoogolapple.bgaannotation.BGAA;
+import cn.bingoogolapple.bgaannotation.BGAALayout;
+import cn.bingoogolapple.bgaannotation.BGAAView;
+
+@BGAALayout(R.layout.activity_splash)
+public class SplashActivity extends FragmentActivity {
+
+    @BGAAView(R.id.banner_splash_pager)
+    private BGABanner mPagerBanner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        BGAA.injectView2Activity(this);
+
+        List<View> views = new ArrayList<View>();
+        views.add(getLayoutInflater().inflate(R.layout.view_one, null));
+        views.add(getLayoutInflater().inflate(R.layout.view_two, null));
+        views.add(getLayoutInflater().inflate(R.layout.view_three, null));
+        View lastView = getLayoutInflater().inflate(R.layout.view_last,null);
+        views.add(lastView);
+        mPagerBanner.setViewPagerViews(views);
+
+        lastView.findViewById(R.id.btn_last_main).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+    }
+}
